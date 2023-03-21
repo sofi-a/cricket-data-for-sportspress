e4bca47a615908884fd2825efd8a71c778115dd0
Sofonias Abathun <sofoniasabathun@gmail.com>
2023-03-21 22:50:55 +0300

    Plugin declaration

---
+++ cricket-data-for-sportspress.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Plugin Name: Cricket Data for SportsPress
+ * Plugin URI: http://github.com/cricket-data-for-sportspress
+ * Description: Cricket Data integration [https://cricketdata.org/how-to-use-cricket-data-api.aspx] for SportsPress
+ * Version: 1.0.0
+ * Author: Sofonias Abathun
+ * Author URI: http://github.com/sofi-a
+ * Requires at least: 3.8
+ * Tested up to: 6.1
+ * Text Domain: cricket-data-for-sportspress
+ * License: GPL3
+ * 
+ * Cricket Data for SportsPress is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 2 of the license, or
+ * any later version.
+ * 
+ * Cricket Data for SportsPress is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU GENERAL PUBLIC LICENSE for more details.
+ * You should have received a copy of the GNU Public License
+ * along with Cricket Data for SportsPress. If not see https://www.gnu.org/licenses/gpl.html.
+ * 
+ * @package Cricket_Data_For_SportsPress
+ * @category API Integration
+ * @author Sofonias Abathun
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
